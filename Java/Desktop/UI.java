0f3ae46ad6a1cee60ff950c21740ffe94c31cf1b
DumbestPerson224 <eTmullall@outlook.com>
2024-11-10 12:38:35 -0500

    Added more features to UI.java

--- Java/Desktop/UI.java
+++ Java/Desktop/UI.java
@@ -1,20 +1,24 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 public class UI{
-     public static JFrame frame;
+     public static JFrame window = new JFrame();
+     public static JButton button = new JButton();
+     public static JTextField textField = new JTextField();
      
+     public static void setVisible (boolean visible){
+          window.setVisible(visible);
+     }
+     
+     public static void addButton(){
+          window.add(button);
+     }
+
+     public static void addTextField(){
+          window.add(textField);
+     }
+
      public static void setSize(int width, int height){
-          frame.setSize(width, height);
-     }
-
-     public static void setVisible(boolean visible){
-          frame.setVisible(visible);
-     }
-
-     public static boolean isVisible(){
-          return frame.isVisible();
-     }
-
-     public static void show(){
-          frame.show();
+          window.setSize(width, height);
      }
 }
